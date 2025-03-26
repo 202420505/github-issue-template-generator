18bfa175103ecbce5e675cf0d95c63bcb3e86163
Wint Khant Lin <96770798+happer64bit@users.noreply.github.com>
2025-03-26 15:31:03 +0000

    add auto removes tag, project, and assignees if empty

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { createSignal, createEffect } from "solid-js";
+import { createSignal, createMemo } from "solid-js";
 import { createStore } from "solid-js/store";
-import { For, Show } from "solid-js";
+import { For } from "solid-js";
 import type { Component } from "solid-js";
 import PlusIcon from "lucide-solid/icons/plus";
-import { CopyIcon } from "lucide-solid";
+import { CopyIcon, TrashIcon } from "lucide-solid";
 import Header from "./components/Header";
 import Button from "./components/ui/Button";
 import Input from "./components/ui/Input";
 import Textarea from "./components/ui/Textarea";
 import Select from "./components/ui/Select";
-import { stringify } from 'yaml';
+import { stringify } from "yaml";
 import { Highlight } from "solid-highlight";
-
-import 'prismjs'
-import 'solid-devtools';
-import '@jongwooo/prism-theme-github/themes/prism-github-default-auto.min.css';
+import "prismjs";
+import "@jongwooo/prism-theme-github/themes/prism-github-default-auto.min.css";
 import "prismjs/components/prism-yaml";
 
 interface Section {
@@ -26,7 +24,6 @@ interface Section {
   options?: string[];
   multiple?: boolean;
   required?: boolean;
-  render?: string;
 }
 
 const App: Component = () => {
@@ -38,67 +35,58 @@ const App: Component = () => {
     project: "",
     assignees: "",
     sections: [{ type: "Markdown", value: "" }] as Section[],
-    yamlOutput: "",
   });
-
 
   const [copied, setCopied] = createSignal(false);
 
   const addSection = () => {
-    setStore("sections", [...store.sections, { type: "Markdown", value: "" }]);
+    setStore("sections", (sections) => [...sections, { type: "Markdown", value: "" }]);
   };
 
-  const handleChange = (index: number, key: keyof Section, value: string | any) => {
-    setStore("sections", index, (prevSection) => ({ ...prevSection, [key]: value }));
+  const removeSection = (index: number) => {
+    setStore("sections", (sections) => sections.filter((_, i) => i !== index));
   };
 
-  const generateYaml = () => {
-    const topFields = {
+  const handleChange = (index: number, updates: Partial<Section>) => {
+    setStore("sections", index, (prev) => ({ ...prev, ...updates }));
+  };
+
+  const yamlOutput = createMemo(() => {
+    const topFields: Partial<Record<keyof typeof store, string>> = {
       name: store.name,
       title: store.title,
       description: store.description,
-      tag: store.tag,
-      project: store.project,
-      assignees: store.assignees
     };
 
-    const formData = store.sections.map((section, index) => {
-      const result: any = { type: section.type.toLowerCase() };
+    (["tag", "project", "assignees"] as const).forEach((key) => {
+      if (store[key] && store[key].trim() !== "") {
+        topFields[key] = store[key] as string;
+      }
+    });
 
-      if (section.type !== "Markdown") {
-        result.id = `${section.type.toLowerCase().replace(/ /g, '-')}-${index}`;
-        result.attributes = {};
+    const formData = store.sections
+      .map((section, index) => {
+        const result: any = { type: section.type.toLowerCase() };
 
-        for (const key in section) {
-          const value = section[key as keyof Section];
-          if (value && key !== "type") {
-            result.attributes[key] = value;
-          }
-        }
-
-        if (section.type === "Dropdown" && section.options) {
-          result.attributes.default = 0;
-        }
-      } else {
-        if (section.value) {
+        if (section.type !== "Markdown") {
+          result.id = `${section.type.toLowerCase().replace(/ /g, "-")}-${index}`;
+          result.attributes = { ...section };
+          delete result.attributes.type;
+        } else if (section.value) {
           result.attributes = { value: section.value };
         }
-      }
 
-      return result;
-    }).filter((item) => Object.keys(item.attributes || {}).length > 0);
+        return result;
+      })
+      .filter((item) => Object.keys(item.attributes || {}).length > 0);
 
     return stringify({ ...topFields, body: formData });
-  };
-
-  createEffect(() => {
-    setStore("yamlOutput", generateYaml());
   });
 
   const handleCopy = async () => {
-    await navigator.clipboard.writeText(store.yamlOutput);
+    await navigator.clipboard.writeText(yamlOutput());
     setCopied(true);
-    setTimeout(() => setCopied(false), 2000); // Reset copied state after 2 seconds
+    setTimeout(() => setCopied(false), 2000);
   };
 
   return (
@@ -107,30 +95,30 @@ const App: Component = () => {
       <div class="container">
         <div class="grid md:grid-cols-2 gap-10">
           <div class="space-y-4 my-4">
-            {["name", "title", "description", "tag", "project", "assignees"].map((field: string) => (
+            {["name", "title", "description", "tag", "project", "assignees"].map((field) => (
               <div class="space-y-1">
                 <label for={`${field}-input`} class="text-sm capitalize">{field}</label>
                 {field === "description" ? (
                   <Textarea
                     id={`${field}-input`}
-                    placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                    placeholder={`Enter ${field}`}
                     rows={4}
                     class="w-full"
-                    value={store[field] as string}
-                    onInput={(e) => setStore(field, e.currentTarget.value)}
+                    value={store[field as keyof typeof store] as string}
+                    onInput={(e) => setStore(field as keyof typeof store, e.currentTarget.value)}
                   />
                 ) : (
                   <Input
                     id={`${field}-input`}
-                    placeholder={field.charAt(0).toUpperCase() + field.slice(1)}
+                    placeholder={`Enter ${field}`}
                     class="w-full"
-                    //@ts-ignore
-                    value={store[field] as string}
-                    onInput={(e) => setStore(field as any, e.currentTarget.value)}
+                    value={store[field as keyof typeof store] as string}
+                    onInput={(e) => setStore(field as keyof typeof store, e.currentTarget.value)}
                   />
                 )}
               </div>
             ))}
+
             <div>
               <h2 class="text-lg">Body</h2>
               <div class="space-y-2 divide-y-[1px] dark:divide-white/20">
@@ -140,107 +128,39 @@ const App: Component = () => {
                       <Select
                         class="block my-4"
                         value={section.type}
-                        onInput={(e) => handleChange(index(), "type", e.currentTarget.value as Section["type"])}
+                        onInput={(e) => handleChange(index(), { type: e.currentTarget.value as Section["type"] })}
                       >
-                        {["Markdown", "Textarea", "Input", "Dropdown", "Checkboxes"].map(type => (
+                        {["Markdown", "Textarea", "Input", "Dropdown", "Checkboxes"].map((type) => (
                           <option value={type}>{type}</option>
                         ))}
                       </Select>
-                      <div class="space-y-2">
-                        <Show when={section.type !== "Markdown"}>
-                          <Input
-                            class="w-full block"
-                            placeholder="Enter label"
-                            value={section.label || ""}
-                            onInput={(e) => handleChange(index(), "label", e.currentTarget.value)}
-                          />
-                          <Input
-                            class="w-full block"
-                            placeholder="Enter description"
-                            value={section.description || ""}
-                            onInput={(e) => handleChange(index(), "description", e.currentTarget.value)}
-                          />
-                          {section.type === "Input" && (
-                            <>
-                              <Input
-                                class="w-full block"
-                                placeholder="Enter placeholder"
-                                value={section.placeholder || ""}
-                                onInput={(e) => handleChange(index(), "placeholder", e.currentTarget.value)}
-                              />
-                              <Input
-                                class="w-full block"
-                                placeholder="Enter value"
-                                value={section.value || ""}
-                                onInput={(e) => handleChange(index(), "value", e.currentTarget.value)}
-                              />
-                            </>
-                          )}
-                          {section.type === "Textarea" && (
-                            <Textarea
-                              rows={3}
-                              class="w-full block"
-                              placeholder="Enter text"
-                              value={section.value || ""}
-                              onInput={(e) => handleChange(index(), "value", e.currentTarget.value)}
-                            />
-                          )}
-                          {section.type === "Dropdown" && (
-                            <Input
-                              class="w-full block"
-                              placeholder="Enter options"
-                              value={section.options?.join(", ") || ""}
-                              onInput={(e) => handleChange(index(), "options", e.currentTarget.value.split(", "))}
-                            />
-                          )}
-                          {section.type === "Checkboxes" && (
-                            <Input
-                              class="w-full block"
-                              placeholder="Enter options"
-                              value={section.options?.join(", ") || ""}
-                              onInput={(e) => handleChange(index(), "options", e.currentTarget.value.split(", "))}
-                            />
-                          )}
-                        </Show>
-                        {section.type === "Markdown" && (
-                          <Textarea
-                            rows={3}
-                            class="w-full block"
-                            placeholder="Enter markdown content"
-                            value={section.value || ""}
-                            onInput={(e) => handleChange(index(), "value", e.currentTarget.value)}
-                          />
-                        )}
+
+                      <div class="flex justify-end">
+                        <Button variety="primary" class="flex gap-1" onClick={() => removeSection(index())}>
+                          <TrashIcon size={14} /> Remove
+                        </Button>
                       </div>
                     </div>
                   )}
                 </For>
+
                 <div class="flex justify-end my-6 py-4">
-                  <Button
-                    variety="primary"
-                    class="flex gap-1 py-1.5 items-center"
-                    onClick={addSection}
-                  >
-                    <PlusIcon size={14} />
-                    Add Section
+                  <Button variety="primary" class="flex gap-1 py-1.5" onClick={addSection}>
+                    <PlusIcon size={14} /> Add Section
                   </Button>
                 </div>
               </div>
             </div>
           </div>
+
           <div class="mt-6">
             <div class="flex justify-end">
-              <button
-                class="px-4 py-1 flex items-center gap-2 text-sm hover:bg-black/10 dark:hover:bg-white/10 rounded transform duration-200"
-                onClick={handleCopy}
-              >
+              <button class="px-4 py-1 flex gap-2 text-sm hover:bg-black/10 rounded" onClick={handleCopy}>
                 {copied() ? "Copied" : "Copy"}
                 <CopyIcon size={17} />
               </button>
             </div>
-            <Highlight language="yaml">
-              {store.yamlOutput}
-            </Highlight>
+            <Highlight language="yaml">{yamlOutput()}</Highlight>
           </div>
         </div>
       </div>
