abe6012eb67a3b2c8c29bb0711886261a05954fb
unknown <happer64bit@gmail.com>
2025-03-29 11:05:02 +0630

    update to partytown

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import devtools from 'solid-devtools/vite'
+import { join } from 'path';
+import { partytownVite } from '@qwik.dev/partytown/utils';
 
 export default defineConfig({
   plugins: [
     solid(),
-    devtools()
+    devtools(),
+    partytownVite({
+      dest: join(__dirname, 'dist', '~partytown'),
+    }),
   ],
   base: "/github-issue-template-generator"
 })
